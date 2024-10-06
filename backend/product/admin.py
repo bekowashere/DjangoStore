2f3cf90b1966e4e81dd30edcd58ebba61a9498da
bekowashere <uberke.karatas@gmail.com>
2024-10-07 01:21:16 +0300

    product admin/ add ProductAdmin

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from product.models import (
     Category,
     ProductType,
+    Product,
     ProductVariant,
     ProductMedia,
     VariantMedia
@@ -21,3 +22,22 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
     search_fields = ('name', 'slug')
     ordering = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (_('Main Information'), {
+            'fields': (
+                'product_type', 'category', 'uuid', 'name', 'slug', 'is_active', 'default_variant'
+            )
+        }),
+        (_('Specifications'), {'fields': ('weight', 'rating')}),
+        (_('Description'), {'fields': ('description',)}),
+        (_('Dates'), {'fields': ('created_at', 'updated_at')}),
+    )
+
+    list_display = ('uuid', 'name', 'slug')
+    list_filter = ('uuid', 'name__icontains', 'slug')
+    search_fields = ('uuid', 'email', 'username')
+    readonly_fields = ('uuid',)
+    ordering = ('created_at',)
