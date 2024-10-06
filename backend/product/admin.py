77699ddb8ff9cb2ce4f3d9ce0a829fda1e34ebfb
bekowashere <uberke.karatas@gmail.com>
2024-10-07 01:14:44 +0300

    product admin/ add CategoryAdmin

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from product.models import (
+    Category,
+    ProductType,
+    ProductVariant,
+    ProductMedia,
+    VariantMedia
+)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (_('Main Information'), {'fields': ('name', 'slug', 'parent')}),
+        (_('Description'), {'fields': ('description',)}),
+        (_('Image'), {'fields': ('background_image', 'background_image_alt')}),
+        (_('Dates'), {'fields': ('updated_at',)}),
+    )
+
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
+    ordering = ('name',)
